9cbbfcacf29959aa0eaa9263d6c740c13b2ee0b0
mingeun0507 <tqdlfkg@naver.com>
2023-07-19 05:42:05 +0900

    fix: HealthCheck URI 수정

--- src/main/java/com/mannavoca/zenga/HealthCheckController.java
+++ src/main/java/com/mannavoca/zenga/HealthCheckController.java
@@ -1,14 +1,15 @@
 package com.mannavoca.zenga;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sample")
-public class SampleController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, world!";
+@RequestMapping("/")
+public class HealthCheckController {
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello, World!");
     }
 }
