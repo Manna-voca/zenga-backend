45062ef1a8ac602f4293fa01059ef4b32e7a5771
99winnmin <ojysep9987@gmail.com>
2023-07-24 21:09:55 +0900

    :bento: [Feat] jpaAuditing, Properties 임베드 어노테이션 추가

--- src/main/java/com/mannavoca/zenga/ZengaApplication.java
+++ src/main/java/com/mannavoca/zenga/ZengaApplication.java
@@ -1,9 +1,14 @@
 package com.mannavoca.zenga;
 
+import com.mannavoca.zenga.common.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableConfigurationProperties({JwtProperties.class})
 public class ZengaApplication {
 
     public static void main(String[] args) {
