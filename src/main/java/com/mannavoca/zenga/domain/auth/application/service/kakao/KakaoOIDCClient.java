c85052257d16233cb032099a6e77cc79426addaf
mingeun0507 <tqdlfkg@naver.com>
2023-08-10 23:14:58 +0900

    [Feat] KAKAO OIDC 구현

---
+++ src/main/java/com/mannavoca/zenga/domain/auth/application/service/kakao/KakaoOIDCClient.java
@@ -0,0 +1,16 @@
+package com.mannavoca.zenga.domain.auth.application.service.kakao;
+
+import com.mannavoca.zenga.common.security.dto.OIDCPublicKeysResponse;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        name = "KakaoOIDCClient",
+        url = "https://kauth.kakao.com"
+)
+public interface KakaoOIDCClient {
+    @Cacheable(cacheNames = "KakaoOICD", cacheManager = "kakaoOidcCacheManager")
+    @GetMapping("/.well-known/jwks.json")
+    OIDCPublicKeysResponse getKakaoOIDCOpenKeys();
+}
