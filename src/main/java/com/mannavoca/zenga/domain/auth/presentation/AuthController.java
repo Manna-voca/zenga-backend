5e3c97e3029ee4486bd432acbf8d1d5e0bce4223
mingeun0507 <tqdlfkg@naver.com>
2023-08-12 20:46:41 +0900

    [Feat] 리프레시 로직 및 API 구현

--- src/main/java/com/mannavoca/zenga/domain/auth/presentation/AuthController.java
+++ src/main/java/com/mannavoca/zenga/domain/auth/presentation/AuthController.java
@@ -1,14 +1,14 @@
 package com.mannavoca.zenga.domain.auth.presentation;
 
 import com.mannavoca.zenga.common.dto.ResponseDto;
+import com.mannavoca.zenga.domain.auth.application.dto.request.RefreshTokensRequestDto;
 import com.mannavoca.zenga.domain.auth.application.dto.response.TokenResponseDto;
 import com.mannavoca.zenga.domain.auth.domain.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +17,12 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/login/kakao")
-    public ResponseEntity<ResponseDto<TokenResponseDto>> loginByKakaoAuthCode(@RequestParam String code) {
+    public ResponseEntity<ResponseDto<TokenResponseDto>> loginByKakaoAuthCode(@NotNull @RequestParam String code) {
         return ResponseEntity.ok(ResponseDto.success(authService.generateTokens(code)));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ResponseDto<TokenResponseDto>> refreshTokens(@RequestBody RefreshTokensRequestDto refreshToken) {
+        return ResponseEntity.ok(ResponseDto.success(authService.refreshTokens(refreshToken)));
     }
 }
