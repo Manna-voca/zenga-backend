f7a84216caaa094327d111cb3e2eb6604307d4d2
99winnmin <ojysep9987@gmail.com>
2023-08-14 18:56:50 +0900

    :rocket: [Fix] MemberPraise에 TimeSection, suffleCnt 칼럼 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/domain/entity/MemberPraise.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/domain/entity/MemberPraise.java
@@ -2,6 +2,7 @@ package com.mannavoca.zenga.domain.praise.domain.entity;
 
 import com.mannavoca.zenga.common.infrastructure.domain.BaseEntity;
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
+import com.mannavoca.zenga.domain.praise.domain.entity.enumType.TimeSectionType;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,12 @@ public class MemberPraise extends BaseEntity {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "time_section")
+    private TimeSectionType timeSection;
+
+    @Column(name = "suffle_count")
+    private int suffleCount;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "praise_member_id")
     private Member praiseMember;
@@ -32,9 +39,15 @@ public class MemberPraise extends BaseEntity {
     private Praise praise;
 
     @Builder
-    public MemberPraise(Member praiseMember, Member praisedMember, Praise praise) {
+    public MemberPraise(TimeSectionType timeSection, Member praiseMember, Member praisedMember, Praise praise) {
+        this.timeSection = timeSection;
+        this.suffleCount = 0;
         this.praiseMember = praiseMember;
         this.praisedMember = praisedMember;
         this.praise = praise;
+    }
+
+    public void updateSuffleCount(){
+        this.suffleCount++;
     }
 }
