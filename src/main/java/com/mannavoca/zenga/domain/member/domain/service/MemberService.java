f16b0947a5c8c9dbc21a46e8af9fc5f889ea3f34
99winnmin <ojysep9987@gmail.com>
2023-08-14 18:58:48 +0900

    :sparkles: [Feat] member 도메인 서비스 및 repo 추가

--- src/main/java/com/mannavoca/zenga/domain/member/domain/service/MemberService.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/service/MemberService.java
@@ -1,4 +1,21 @@
 package com.mannavoca.zenga.domain.member.domain.service;
 
+import com.mannavoca.zenga.common.annotation.DomainService;
+import com.mannavoca.zenga.common.exception.BusinessException;
+import com.mannavoca.zenga.common.exception.Error;
+import com.mannavoca.zenga.domain.member.domain.entity.Member;
+import com.mannavoca.zenga.domain.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@DomainService
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member findMemberByUserId(Long userId, Long channelId) {
+        return memberRepository.findMemberByUser_IdAndClub_Id(userId, channelId)
+                .orElseThrow(() -> BusinessException.of(Error.DATA_NOT_FOUND));
+    }
 }
