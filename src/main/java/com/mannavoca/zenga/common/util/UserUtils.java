cf747705f941783109c74080b48395e1f3c4825d
99winnmin <ojysep9987@gmail.com>
2023-08-14 18:57:38 +0900

    :sparkles: [Feat] UserUtils 에 user, member 가져오는 util 추가

--- src/main/java/com/mannavoca/zenga/common/util/UserUtils.java
+++ src/main/java/com/mannavoca/zenga/common/util/UserUtils.java
@@ -1,10 +1,25 @@
 package com.mannavoca.zenga.common.util;
 
+import com.mannavoca.zenga.domain.member.domain.entity.Member;
+import com.mannavoca.zenga.domain.member.domain.service.MemberService;
+import com.mannavoca.zenga.domain.user.domain.entity.User;
+import com.mannavoca.zenga.domain.user.domain.service.UserFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserUtils {
+    private final UserFindService userFindService;
+    private final MemberService memberService;
 
+    public User getUser(){
+        final Long userId = SecurityUtils.getUserId();
+        return userFindService.findById(userId);
+    }
+
+    public Member getMember(Long channelId) {
+        final Long userId = SecurityUtils.getUserId();
+        return memberService.findMemberByUserId(userId, channelId);
+    }
 }
