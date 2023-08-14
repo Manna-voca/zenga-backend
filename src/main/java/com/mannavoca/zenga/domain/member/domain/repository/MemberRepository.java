5306f7ad8a45fc6ad9779adb646163cf404409a9
99winnmin <ojysep9987@gmail.com>
2023-08-14 18:58:48 +0900

    :sparkles: [Feat] member 도메인 서비스 및 repo 추가

--- src/main/java/com/mannavoca/zenga/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.mannavoca.zenga.domain.member.domain.repository;
 
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findMemberByUser_IdAndClub_Id(Long userId, Long clubId);
 }
