7743e2e7371bf90e1453b03cb89b75cddba52096
99winnmin <ojysep9987@gmail.com>
2023-08-20 18:55:49 +0900

    :sparkles: Feat : 받은 칭찬, 보낸 칭찬 페이지네이션 API 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/presentation/PraiseGetController.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/presentation/PraiseGetController.java
@@ -1,7 +1,10 @@
 package com.mannavoca.zenga.domain.praise.presentation;
 
+import com.mannavoca.zenga.common.dto.PageResponse;
 import com.mannavoca.zenga.common.dto.ResponseDto;
 import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.ReceivedPraiseInfoResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.SendPraiseInfoResponseDto;
 import com.mannavoca.zenga.domain.praise.application.service.PraiseSearchUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,5 +26,27 @@ public class PraiseGetController {
     {
         CurrentTodoPraiseResponseDto currentTodoPraiseResponseDto = praiseSearchUseCase.getCurrentTodoPraiseAndMemberList(channelId);
         return ResponseEntity.ok(ResponseDto.of(200, "칭찬 보내기 데이터 조ㅅ에 성공했습니다.", currentTodoPraiseResponseDto));
+    }
+
+    @GetMapping("/receive")
+    public ResponseEntity<ResponseDto<PageResponse<ReceivedPraiseInfoResponseDto>>> getReceivedPraiseList
+            (
+                    @RequestParam Long channelId,
+                    @RequestParam int page
+            )
+    {
+        PageResponse<ReceivedPraiseInfoResponseDto> pageResponse = praiseSearchUseCase.getReceivedPraiseList(channelId, page);
+        return ResponseEntity.ok(ResponseDto.of(200, "받은 칭찬 리스트 조회에 성공했습니다.", pageResponse));
+    }
+
+    @GetMapping("/send")
+    public ResponseEntity<ResponseDto<PageResponse<SendPraiseInfoResponseDto>>> getDonePraiseList
+            (
+                    @RequestParam Long channelId,
+                    @RequestParam int page
+            )
+    {
+        PageResponse<SendPraiseInfoResponseDto> pageResponse = praiseSearchUseCase.getSendPraiseList(channelId, page);
+        return ResponseEntity.ok(ResponseDto.of(200, "보낸 칭찬 리스트 조회에 성공했습니다.", pageResponse));
     }
 }
