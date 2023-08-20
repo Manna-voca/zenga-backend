595d6fb373233a358b5ccfc7d2858366a6631540
99winnmin <ojysep9987@gmail.com>
2023-08-20 19:25:30 +0900

    :necktie: Feat : 칭찬보내기 비지니스 로직 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/application/service/PraiseUpdateUseCase.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/application/service/PraiseUpdateUseCase.java
@@ -4,6 +4,7 @@ import com.mannavoca.zenga.common.annotation.UseCase;
 import com.mannavoca.zenga.common.util.UserUtils;
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import com.mannavoca.zenga.domain.member.domain.service.MemberService;
+import com.mannavoca.zenga.domain.praise.application.dto.request.ChooseMemberPraiseRequestDto;
 import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
 import com.mannavoca.zenga.domain.praise.application.mapper.PraiseMapper;
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
@@ -17,13 +18,13 @@ import java.util.Optional;
 
 @Slf4j
 @UseCase
+@Transactional
 @RequiredArgsConstructor
 public class PraiseUpdateUseCase {
     private final UserUtils userUtils;
     private final MemberPraiseService memberPraiseService;
     private final MemberService memberService;
 
-    @Transactional
     public CurrentTodoPraiseResponseDto getAgainCurrentTodoPraiseAndMemberList(Long channelId) {
         Member member = userUtils.getMember(channelId);
         Optional<MemberPraise> todayTodoPraiseOpt = memberPraiseService.findTodayTodoPraiseForMember(member.getId());
@@ -35,5 +36,12 @@ public class PraiseUpdateUseCase {
                     return PraiseMapper.mapToCurrentTodoPraiseResponseDto(todayTodoPraise, randomMembersByChannelId);
                 })
                 .orElse(null);
+    }
+
+    public void choosePraise(ChooseMemberPraiseRequestDto chooseMemberPraiseRequestDto) {
+        Member praisedMember = memberService.findMemberById(chooseMemberPraiseRequestDto.getPraisedMemberId());
+        MemberPraise memberPraiseById = memberPraiseService.findMemberPraiseById(chooseMemberPraiseRequestDto.getMemberPraiseId());
+
+        memberPraiseService.updatePraisedMemberToMemberPraise(memberPraiseById, praisedMember);
     }
 }
