6b5d359b51f90dabc5b54efcd3030be740882b50
99winnmin <ojysep9987@gmail.com>
2023-08-20 18:59:36 +0900

    :rocket: Fix : 받은 칭찬, 보낸 칭찬 조회 offset 페이지네이션 쿼리 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/infrastructure/MemberPraiseRepositoryImpl.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/infrastructure/MemberPraiseRepositoryImpl.java
@@ -3,8 +3,12 @@ package com.mannavoca.zenga.domain.praise.infrastructure;
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
 import com.mannavoca.zenga.domain.praise.domain.entity.enumType.TimeSectionType;
 import com.mannavoca.zenga.domain.praise.domain.repository.MemberPraiseRepositoryCustom;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -50,5 +54,51 @@ public class MemberPraiseRepositoryImpl implements MemberPraiseRepositoryCustom 
                                 .and(memberPraise.createdDate.month().eq(now.getMonthValue()))
                                 .and(memberPraise.createdDate.dayOfMonth().eq(now.getDayOfMonth()))
                 ).fetchOne());
+    }
+
+    @Override
+    public Page<MemberPraise> getReceivedPraiseList(Long memberId, Pageable pageable) {
+        List<MemberPraise> memberPraiseList = queryFactory
+                .selectFrom(memberPraise)
+                .where(
+                        memberPraise.praisedMember.isNotNull()
+                                .and(memberPraise.praisedMember.id.eq(memberId))
+                )
+                .orderBy(memberPraise.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        JPAQuery<Long> countQuery = queryFactory
+                .select(memberPraise.count())
+                .from(memberPraise)
+                .where(
+                        memberPraise.praisedMember.isNotNull()
+                                .and(memberPraise.praisedMember.id.eq(memberId))
+                );
+        return PageableExecutionUtils.getPage(memberPraiseList, pageable, countQuery::fetchOne);
+    }
+
+    @Override
+    public Page<MemberPraise> getMyCompletePraiseList(Long memberId, Pageable pageable) {
+        List<MemberPraise> memberPraiseList = queryFactory
+                .selectFrom(memberPraise)
+                .where(
+                        memberPraise.praisedMember.isNotNull()
+                                .and(memberPraise.praiseMember.id.eq(memberId))
+                )
+                .orderBy(memberPraise.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(memberPraise.count())
+                .from(memberPraise)
+                .where(
+                        memberPraise.praisedMember.isNotNull()
+                                .and(memberPraise.praiseMember.id.eq(memberId))
+                );
+
+        return PageableExecutionUtils.getPage(memberPraiseList, pageable, countQuery::fetchOne);
     }
 }
