082949c26f712e1c61845b8f5a2da4507c0e5279
99winnmin <ojysep9987@gmail.com>
2023-08-21 05:09:33 +0900

    :sparkles: Feat : Participation, Party Repository 추가

--- src/main/java/com/mannavoca/zenga/domain/party/domain/repository/ParticipationRepository.java
+++ src/main/java/com/mannavoca/zenga/domain/party/domain/repository/ParticipationRepository.java
@@ -3,5 +3,8 @@ package com.mannavoca.zenga.domain.party.domain.repository;
 import com.mannavoca.zenga.domain.party.domain.entity.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+    List<Participation> findParticipationByParty_Id(Long partyId);
 }
--- src/main/java/com/mannavoca/zenga/domain/party/domain/repository/PartyRepository.java
+++ src/main/java/com/mannavoca/zenga/domain/party/domain/repository/PartyRepository.java
@@ -3,5 +3,5 @@ package com.mannavoca.zenga.domain.party.domain.repository;
 import com.mannavoca.zenga.domain.party.domain.entity.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PartyRepository extends JpaRepository<Party, Long> {
+public interface PartyRepository extends JpaRepository<Party, Long>, PartyRepositoryCustom {
 }
