534a5cb5d25df9713c17b25fc3b4277aacf5ba3a
99winnmin <ojysep9987@gmail.com>
2023-08-20 19:00:21 +0900

    :necktie: Fix : 받은 칭찬, 보낸 칭찬 조회 비지니스 로직 및 mapper 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/application/mapper/PraiseMapper.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/application/mapper/PraiseMapper.java
@@ -1,9 +1,14 @@
 package com.mannavoca.zenga.domain.praise.application.mapper;
 
 import com.mannavoca.zenga.common.annotation.Mapper;
+import com.mannavoca.zenga.common.dto.PageResponse;
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.ReceivedPraiseInfoResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.SendPraiseInfoResponseDto;
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,8 +26,32 @@ public class PraiseMapper {
                 ).collect(Collectors.toList());
         return CurrentTodoPraiseResponseDto.builder()
                 .praise(todayTodoPraiseForMember.getPraise().getDescription())
-                .shuffleCount(todayTodoPraiseForMember.getSuffleCount())
+                .shuffleCount(todayTodoPraiseForMember.getShuffleCount())
                 .memberList(memberInfoLists)
                 .build();
+    }
+
+    public static PageResponse<ReceivedPraiseInfoResponseDto> mapToReceivedPraiseInfoResponseDto(Page<MemberPraise> receivedPraiseList) {
+        List<ReceivedPraiseInfoResponseDto> responseDtos = receivedPraiseList.getContent().stream()
+                .map(memberPraise -> ReceivedPraiseInfoResponseDto.builder()
+                        .receivedPraiseId(memberPraise.getId())
+                        .praiseDescription(memberPraise.getPraise().getDescription())
+                        .memberName(memberPraise.getIsOpen() ? memberPraise.getPraiseMember().getNickname() : "익명")
+                        .memberProfileImageUrl(memberPraise.getIsOpen() ? memberPraise.getPraiseMember().getProfileImageUrl() : null)
+                        .build()
+                ).collect(Collectors.toList());
+        return PageResponse.of(new PageImpl<>(responseDtos, receivedPraiseList.getPageable(), receivedPraiseList.getTotalElements()));
+    }
+
+    public static PageResponse<SendPraiseInfoResponseDto> mapToSendPraiseInfoResponseDto(Page<MemberPraise> myCompletePraiseList) {
+        List<SendPraiseInfoResponseDto> responseDtos = myCompletePraiseList.getContent().stream()
+                .map(memberPraise -> SendPraiseInfoResponseDto.builder()
+                        .donePraiseId(memberPraise.getId())
+                        .praiseDescription(memberPraise.getPraise().getDescription())
+                        .memberName(memberPraise.getPraisedMember().getNickname())
+                        .memberProfileImageUrl(memberPraise.getPraisedMember().getProfileImageUrl())
+                        .build()
+                ).collect(Collectors.toList());
+        return PageResponse.of(new PageImpl<>(responseDtos, myCompletePraiseList.getPageable(), myCompletePraiseList.getTotalElements()));
     }
 }
