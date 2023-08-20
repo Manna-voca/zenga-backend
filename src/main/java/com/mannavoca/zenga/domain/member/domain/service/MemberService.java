00a4a107b0550faba678c3a02d82a78f55491bbb
99winnmin <ojysep9987@gmail.com>
2023-08-20 14:51:37 +0900

    :necktie: Feat : 동아리에서 본인 제외 랜덤 4명 조회 도메인 메서드 추가

--- src/main/java/com/mannavoca/zenga/domain/member/domain/service/MemberService.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/service/MemberService.java
@@ -8,6 +8,9 @@ import com.mannavoca.zenga.domain.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collections;
+import java.util.List;
+
 @Slf4j
 @DomainService
 @RequiredArgsConstructor
@@ -17,5 +20,13 @@ public class MemberService {
     public Member findMemberByUserId(Long userId, Long channelId) {
         return memberRepository.findMemberByUser_IdAndClub_Id(userId, channelId)
                 .orElseThrow(() -> BusinessException.of(Error.DATA_NOT_FOUND));
+    }
+
+    public List<Member> find4RandomMembersByChannelId(Long memberId, Long channelId) {
+        List<Member> membersByClubId = memberRepository.findMembersByClubId(memberId, channelId);
+
+        Collections.shuffle(membersByClubId);  // 리스트를 랜덤하게 섞는다.
+        // 만약 리스트의 크기가 4 보다 작다면, 리스트의 모든 멤버를 반환한다.
+        return membersByClubId.subList(0, Math.min(membersByClubId.size(), 4));
     }
 }
