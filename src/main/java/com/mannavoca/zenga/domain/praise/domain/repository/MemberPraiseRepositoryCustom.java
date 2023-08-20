63bb7d8a85584770c20eaec514130e76fe091a80
99winnmin <ojysep9987@gmail.com>
2023-08-20 14:53:40 +0900

    :bricks: Feat : 해당 동아리원이 현재 시간에 해야할 칭찬 조회 쿼리 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/domain/repository/MemberPraiseRepositoryCustom.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/domain/repository/MemberPraiseRepositoryCustom.java
@@ -3,7 +3,10 @@ package com.mannavoca.zenga.domain.praise.domain.repository;
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberPraiseRepositoryCustom {
     List<MemberPraise> findAllNotPraised();
+
+    Optional<MemberPraise> findTodayTodoPraise(Long memberId);
 }
