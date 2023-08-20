b0802a753030dcb7815f2a062c3c3efb6daae88f
99winnmin <ojysep9987@gmail.com>
2023-08-20 18:59:36 +0900

    :rocket: Fix : 받은 칭찬, 보낸 칭찬 조회 offset 페이지네이션 쿼리 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/domain/repository/MemberPraiseRepositoryCustom.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/domain/repository/MemberPraiseRepositoryCustom.java
@@ -1,6 +1,8 @@
 package com.mannavoca.zenga.domain.praise.domain.repository;
 
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,8 @@ public interface MemberPraiseRepositoryCustom {
     List<MemberPraise> findAllNotPraised();
 
     Optional<MemberPraise> findTodayTodoPraise(Long memberId);
+
+    Page<MemberPraise> getReceivedPraiseList(Long memberId, Pageable pageable);
+
+    Page<MemberPraise> getMyCompletePraiseList(Long memberId, Pageable pageable);
 }
