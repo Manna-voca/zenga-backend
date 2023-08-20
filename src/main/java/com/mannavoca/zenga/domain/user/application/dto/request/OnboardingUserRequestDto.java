5a22f68e1fb5bf00809db9df1ca5c14fe4e10428
mingeun0507 <tqdlfkg@naver.com>
2023-08-20 17:41:43 +0900

    [Feat] User API 관련 DTO 구성

---
+++ src/main/java/com/mannavoca/zenga/domain/user/application/dto/request/OnboardingUserRequestDto.java
@@ -0,0 +1,24 @@
+package com.mannavoca.zenga.domain.user.application.dto.request;
+
+import com.mannavoca.zenga.domain.user.domain.entity.enumType.GenderType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class OnboardingUserRequestDto {
+
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
+    private String name;
+    @NotNull(message = "성별을 입력해주세요.")
+    private GenderType gender;
+    @DateTimeFormat(pattern = "yyyy-M-d")
+    @NotNull(message = "생년월일을 입력해주세요.")
+    private LocalDate birthDate;
+
+}
--- src/main/java/com/mannavoca/zenga/domain/user/application/dto/response/UserInfoResponseDto.java
+++ src/main/java/com/mannavoca/zenga/domain/user/application/dto/response/UserInfoResponseDto.java
@@ -1,44 +1,26 @@
 package com.mannavoca.zenga.domain.user.application.dto.response;
 
-import com.mannavoca.zenga.domain.user.domain.entity.User;
 import com.mannavoca.zenga.domain.user.domain.entity.enumType.GenderType;
-import com.mannavoca.zenga.domain.user.domain.entity.enumType.RoleType;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class UserInfoResponseDto {
     private Long id;
-    private RoleType role;
     private String name;
     private GenderType gender;
     private LocalDate birth;
-    private String socialId;
 
-    @Builder
-    public UserInfoResponseDto(Long id, RoleType role, String name, GenderType gender, LocalDate birth, String socialId) {
-        this.id = id;
-        this.role = role;
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this.socialId = socialId;
-    }
+    public String getBirth() {
+        return Optional.ofNullable(birth)
+                .map(b -> b.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+                .orElse(null);
 
-    public static UserInfoResponseDto of(User user) {
-        return UserInfoResponseDto
-                .builder()
-                .id(user.getId())
-                .role(user.getRole())
-                .name(user.getName())
-                .gender(user.getGender())
-                .birth(user.getBirth())
-                .socialId(user.getSocialId())
-                .build();
     }
 }
