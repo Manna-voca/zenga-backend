e3a9b3f90b415565a49af19312df412035b6fa1e
99winnmin <ojysep9987@gmail.com>
2023-08-21 02:35:53 +0900

    :sparkles: Feat : Party, Participation Repository 추가

---
+++ src/main/java/com/mannavoca/zenga/domain/party/domain/repository/PartyRepository.java
@@ -0,0 +1,7 @@
+package com.mannavoca.zenga.domain.party.domain.repository;
+
+import com.mannavoca.zenga.domain.party.domain.entity.Party;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyRepository extends JpaRepository<Party, Long> {
+}
