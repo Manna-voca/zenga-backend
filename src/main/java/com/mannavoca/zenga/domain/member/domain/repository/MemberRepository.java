3da93526ab2edc488a955d7d7b24a3db4bf38de6
99winnmin <ojysep9987@gmail.com>
2023-08-20 14:50:21 +0900

    :bricks: Feat : 동아리에서 본인 제외한 Member 가져오는 쿼리 추가

--- src/main/java/com/mannavoca/zenga/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/repository/MemberRepository.java
@@ -4,6 +4,6 @@ import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     Optional<Member> findMemberByUser_IdAndClub_Id(Long userId, Long clubId);
 }
