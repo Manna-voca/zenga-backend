0a57050e64d77956a71a0f0525578848a00eefd3
99winnmin <ojysep9987@gmail.com>
2023-08-20 19:00:21 +0900

    :necktie: Fix : 받은 칭찬, 보낸 칭찬 조회 비지니스 로직 및 mapper 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/application/service/PraiseSearchUseCase.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/application/service/PraiseSearchUseCase.java
@@ -1,15 +1,20 @@
 package com.mannavoca.zenga.domain.praise.application.service;
 
 import com.mannavoca.zenga.common.annotation.UseCase;
+import com.mannavoca.zenga.common.dto.PageResponse;
 import com.mannavoca.zenga.common.util.UserUtils;
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import com.mannavoca.zenga.domain.member.domain.service.MemberService;
 import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.ReceivedPraiseInfoResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.SendPraiseInfoResponseDto;
 import com.mannavoca.zenga.domain.praise.application.mapper.PraiseMapper;
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
 import com.mannavoca.zenga.domain.praise.domain.service.MemberPraiseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,5 +37,19 @@ public class PraiseSearchUseCase {
                     return PraiseMapper.mapToCurrentTodoPraiseResponseDto(todayTodoPraise, randomMembersByChannelId);
                 })
                 .orElse(null);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<ReceivedPraiseInfoResponseDto> getReceivedPraiseList(Long channelId, int page) {
+        Member member = userUtils.getMember(channelId);
+        Page<MemberPraise> receivedPraiseList = memberPraiseService.findReceivedPraiseList(member.getId(), page);
+        return PraiseMapper.mapToReceivedPraiseInfoResponseDto(receivedPraiseList);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<SendPraiseInfoResponseDto> getSendPraiseList(Long channelId, int page) {
+        Member member = userUtils.getMember(channelId);
+        Page<MemberPraise> myCompletePraiseList = memberPraiseService.findMyCompletePraiseList(member.getId(), page);
+        return PraiseMapper.mapToSendPraiseInfoResponseDto(myCompletePraiseList);
     }
 }
