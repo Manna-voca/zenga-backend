62547d775f650dde0a8d926e676701893f8f9fe4
99winnmin <ojysep9987@gmail.com>
2023-08-20 19:00:31 +0900

    :necktie: Fix : 받은 칭찬, 보낸 칭찬 조회 도메인 메서드 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/domain/service/MemberPraiseService.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/domain/service/MemberPraiseService.java
@@ -5,6 +5,9 @@ import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
 import com.mannavoca.zenga.domain.praise.domain.repository.MemberPraiseRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -21,5 +24,15 @@ public class MemberPraiseService {
     public void updatePlusShuffleCountMemberPraise(MemberPraise memberPraise) {
         memberPraise.updateShuffleCount();
         memberPraiseRepository.save(memberPraise);
+    }
+
+    public Page<MemberPraise> findReceivedPraiseList(Long memberId, int page) {
+        Pageable pageable = PageRequest.of(page-1, 20);
+        return memberPraiseRepository.getReceivedPraiseList(memberId, pageable);
+    }
+
+    public Page<MemberPraise> findMyCompletePraiseList(Long memberId, int page) {
+        Pageable pageable = PageRequest.of(page-1, 20);
+        return memberPraiseRepository.getMyCompletePraiseList(memberId, pageable);
     }
 }
