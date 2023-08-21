cb35d6357f9aa0c9d0828f2bd79a64d6ded886c9
99winnmin <ojysep9987@gmail.com>
2023-08-21 13:42:39 +0900

    :sparkles: Feat : Participation 관련 JPA 추가

--- src/main/java/com/mannavoca/zenga/domain/party/domain/repository/ParticipationRepository.java
+++ src/main/java/com/mannavoca/zenga/domain/party/domain/repository/ParticipationRepository.java
@@ -7,4 +7,12 @@ import java.util.List;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
     List<Participation> findParticipationByParty_Id(Long partyId);
+
+    boolean existsByParty_IdAndMember_Id(Long partyId, Long memberId);
+
+    void deleteByParty_IdAndMember_Id(Long partyId, Long memberId);
+
+    void deleteAllByParty_Id(Long partyId);
+
+    Long countByParty_Id(Long partyId);
 }
