3a85cdba5a79e0e10aed0784ad5ad704cfb6d52a
99winnmin <ojysep9987@gmail.com>
2023-08-21 13:46:20 +0900

    :sparkles: Feat : 모임 완료 요청/응답 DTO 추가

---
+++ src/main/java/com/mannavoca/zenga/domain/party/application/dto/request/CompletePartyRequestDto.java
@@ -0,0 +1,12 @@
+package com.mannavoca.zenga.domain.party.application.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CompletePartyRequestDto {
+    private Long partyId;
+    private String partyCardImageUrl;
+}
