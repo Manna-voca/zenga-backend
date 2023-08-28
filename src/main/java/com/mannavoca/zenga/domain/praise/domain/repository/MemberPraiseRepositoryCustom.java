0c81f1ebaf8f23e5d59ef84425e815b66a7f4926
99winnmin <ojysep9987@gmail.com>
2023-08-29 03:01:01 +0900

    :art: Fix : findTodayTodoPraise -> findCurrentTodoPraise 쿼리명 변경

--- src/main/java/com/mannavoca/zenga/domain/praise/domain/repository/MemberPraiseRepositoryCustom.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/domain/repository/MemberPraiseRepositoryCustom.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface MemberPraiseRepositoryCustom {
     List<MemberPraise> findAllNotPraised();
 
-    Optional<MemberPraise> findTodayTodoPraise(Long memberId);
+    Optional<MemberPraise> findCurrentTodoPraise(Long memberId);
 
     Page<MemberPraise> getReceivedPraiseList(Long memberId, Pageable pageable);
 
