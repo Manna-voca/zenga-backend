c09231cc2f625449afe49186237486babe9f767e
99winnmin <ojysep9987@gmail.com>
2023-08-29 03:06:24 +0900

    :sparkles: Feat : 받은 칭찬 익명 풀기 API 추가

--- src/main/java/com/mannavoca/zenga/domain/praise/presentation/PraisePatchController.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/presentation/PraisePatchController.java
@@ -1,14 +1,18 @@
 package com.mannavoca.zenga.domain.praise.presentation;
 
-import com.mannavoca.zenga.common.ResponseCode.ResponseCode;
 import com.mannavoca.zenga.common.dto.ResponseDto;
 import com.mannavoca.zenga.domain.praise.application.dto.request.ChooseMemberPraiseRequestDto;
+import com.mannavoca.zenga.domain.praise.application.dto.request.OpenMemberPraiseRequestDto;
+import com.mannavoca.zenga.domain.praise.application.dto.request.PraiseChannelIdRequestDto;
 import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
 import com.mannavoca.zenga.domain.praise.application.service.PraiseUpdateUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -18,9 +22,9 @@ public class PraisePatchController {
     private final PraiseUpdateUseCase praiseUpdateUseCase;
 
     @PatchMapping("/todo")
-    public ResponseEntity<ResponseDto<CurrentTodoPraiseResponseDto>> getMyCurrentToDoPraiseShuffle(@RequestParam Long channelId)
+    public ResponseEntity<ResponseDto<CurrentTodoPraiseResponseDto>> getMyCurrentToDoPraiseShuffle(@RequestBody PraiseChannelIdRequestDto praiseChannelIdRequestDto)
     {
-        CurrentTodoPraiseResponseDto currentTodoPraiseResponseDto = praiseUpdateUseCase.getAgainCurrentTodoPraiseAndMemberList(channelId);
+        CurrentTodoPraiseResponseDto currentTodoPraiseResponseDto = praiseUpdateUseCase.getAgainCurrentTodoPraiseAndMemberList(praiseChannelIdRequestDto.getChannelId());
         return ResponseEntity.ok(ResponseDto.success(currentTodoPraiseResponseDto));
     }
 
@@ -31,6 +35,16 @@ public class PraisePatchController {
             )
     {
         praiseUpdateUseCase.choosePraise(chooseMemberPraiseRequestDto);
+        return ResponseEntity.ok(ResponseDto.success());
+    }
+
+    @PatchMapping("/open")
+    public ResponseEntity<ResponseDto<Void>> openMemberPraise
+            (
+                    @RequestBody OpenMemberPraiseRequestDto openMemberPraiseRequestDto
+            )
+    {
+        praiseUpdateUseCase.openMemberPraise(openMemberPraiseRequestDto);
         return ResponseEntity.ok(ResponseDto.success());
     }
 }
