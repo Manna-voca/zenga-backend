416022066d1a9b0088598b897c43264ffc36a497
bjih1999 <bjih1999@naver.com>
2023-08-28 23:56:36 +0900

    feat: paticipation text 컬럼 추가

--- src/main/java/com/mannavoca/zenga/domain/party/domain/entity/Participation.java
+++ src/main/java/com/mannavoca/zenga/domain/party/domain/entity/Participation.java
@@ -40,11 +40,15 @@ public class Participation extends BaseEntity {
     @Column(name = "is_private")
     private boolean isPrivate;
 
+    @Column(name = "text")
+    private String text;
+
     @Builder
-    public Participation(Boolean isMaker, Member member, Party party) {
+    public Participation(Boolean isMaker, Member member, Party party, String text) {
         this.isMaker = isMaker;
         this.member = member;
         this.party = party;
         this.isPrivate = false;
+        this.text = text;
     }
 }
