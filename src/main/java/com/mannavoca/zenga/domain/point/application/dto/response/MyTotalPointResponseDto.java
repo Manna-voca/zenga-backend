849491428995c956044994b17479a4c0fbaee9fe
99winnmin <ojysep9987@gmail.com>
2023-08-29 03:08:56 +0900

    :necktie: Feat : point 합산, 내역 조회 responseDto 추가

---
+++ src/main/java/com/mannavoca/zenga/domain/point/application/dto/response/MyTotalPointResponseDto.java
@@ -0,0 +1,14 @@
+package com.mannavoca.zenga.domain.point.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyTotalPointResponseDto {
+    private Integer point;
+}
