132c53e7e0cd4586bb6b93d079e8e131a243b25a
mingeun0507 <tqdlfkg@naver.com>
2023-08-29 11:02:39 +0900

    [Feat] 나의 모임 조회 쿼리 구현

--- src/main/java/com/mannavoca/zenga/domain/party/domain/repository/PartyRepositoryCustom.java
+++ src/main/java/com/mannavoca/zenga/domain/party/domain/repository/PartyRepositoryCustom.java
@@ -1,9 +1,16 @@
 package com.mannavoca.zenga.domain.party.domain.repository;
 
+import com.mannavoca.zenga.domain.member.domain.entity.enumType.State;
 import com.mannavoca.zenga.domain.party.domain.entity.Party;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
+import java.util.List;
+
 public interface PartyRepositoryCustom {
     Slice<Party> findPartyListByChannelId(Long channelId, Long partyId, Pageable pageable);
+
+    List<Party> find2EachPartiesByMemberId(Long memberId);
+
+    Slice<Party> findPartiesByMemberIdAndState(Long memberId, State state, Long partyIdCursor, Pageable pageable);
 }
