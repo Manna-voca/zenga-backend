46eb9b6453f223d163a5db87c7405bda2053cca5
mingeun0507 <tqdlfkg@naver.com>
2023-08-29 17:53:05 +0900

    [Feat] Auth 도메인 에러 추가 정의 및 Validation 구현

--- src/main/java/com/mannavoca/zenga/common/exception/Error.java
+++ src/main/java/com/mannavoca/zenga/common/exception/Error.java
@@ -18,6 +18,7 @@ public enum Error {
     GOOGLE_OAUTH_FAILED2(HttpStatus.BAD_REQUEST,"Google에서 사용자 정보를 검색하지 못했습니다.", 901),
     KAKAO_OAUTH_FAILED(HttpStatus.BAD_REQUEST,"KAKAO 토큰이 만료되었습니다.", 910),
     KAKAO_OAUTH_FAILED2(HttpStatus.BAD_REQUEST,"KAKAO 토큰이 올바르지 않습니다.", 911),
+    KAKAO_OAUTH_FAILED3(HttpStatus.INTERNAL_SERVER_ERROR, "KAKAO 공개 서버와의 통신이 불안정합니다. 다시 시도해주세요.", 912),
     APPLE_OIDC_FAILED(HttpStatus.BAD_REQUEST,"Apple OAuth Identity Token 형식이 올바르지 않습니다.", 920),
     APPLE_OIDC_FAILED2(HttpStatus.BAD_REQUEST,"Apple OAuth 로그인 중 Identity Token 유효기간이 만료됐습니다.", 921),
     APPLE_OIDC_FAILED3(HttpStatus.BAD_REQUEST,"Apple OAuth Identity Token 값이 올바르지 않습니다.", 922),
@@ -48,9 +49,13 @@ public enum Error {
     // Party
     PARTY_NOT_FOUND(HttpStatus.NOT_FOUND, "모임을 찾을 수 없습니다.", 1400),
 
+    // Validation
+    INVALID_API_INPUT_PARAMETER(HttpStatus.BAD_REQUEST,"API 유효성 검증에 실패했습니다. Parameter를 확인해주세요.", 2000),
+    INVALID_API_INPUT_BODY(HttpStatus.BAD_REQUEST,"API 유효성 검증에 실패했습니다. Body를 확인해주세요.", 2001),
+
     // JWT
     INVALID_TOKEN(HttpStatus.BAD_REQUEST,"유효하지 않은 토큰입니다.", 7000),
-    EXPIRED_TOKEN(HttpStatus.BAD_REQUEST,"토큰이 만료되었습니다. 토큰 재발행 혹은 로그인을 다시 해주세요", 7001);
+    EXPIRED_TOKEN(HttpStatus.BAD_REQUEST,"토큰이 만료되었습니다. 토큰 재발행 혹은 로그인을 다시 해주세요.", 7001);
 
     private final HttpStatus httpStatus;
     private final String message;
