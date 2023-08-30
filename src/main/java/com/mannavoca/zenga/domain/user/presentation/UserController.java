aeb81bbf244e9527655384e7d477904d5889c5db
mingeun0507 <tqdlfkg@naver.com>
2023-08-31 01:56:04 +0900

    [Fix] 기획안 수정에 따라 유저 온보딩 -> 유저 정보 수정 API로 변경

--- src/main/java/com/mannavoca/zenga/domain/user/presentation/UserController.java
+++ src/main/java/com/mannavoca/zenga/domain/user/presentation/UserController.java
@@ -1,30 +1,32 @@
 package com.mannavoca.zenga.domain.user.presentation;
 
 import com.mannavoca.zenga.common.dto.ResponseDto;
-import com.mannavoca.zenga.common.util.SecurityUtils;
-import com.mannavoca.zenga.domain.user.application.dto.request.UserOnboardingRequestDto;
+import com.mannavoca.zenga.domain.user.application.dto.request.UpdatingUserInfoRequestDto;
 import com.mannavoca.zenga.domain.user.application.dto.response.UserInfoResponseDto;
 import com.mannavoca.zenga.domain.user.application.service.UserReadUseCase;
 import com.mannavoca.zenga.domain.user.application.service.UserUpdateUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
+@Validated
 public class UserController {
     private final UserReadUseCase userReadUseCase;
     private final UserUpdateUseCase userUpdateUseCase;
 
-
     @GetMapping("/info")
     public ResponseEntity<ResponseDto<UserInfoResponseDto>> getUserInfo() {
-        return ResponseEntity.ok(ResponseDto.success(userReadUseCase.getUserInfo(SecurityUtils.getUserId())));
+        return ResponseEntity.ok(ResponseDto.success(userReadUseCase.getUserInfo()));
     }
 
-    @PutMapping("/onboard")
-    public ResponseEntity<ResponseDto<UserInfoResponseDto>> onboardUser(@RequestBody UserOnboardingRequestDto userOnboardingRequestDto) {
-        return ResponseEntity.ok(ResponseDto.success(userUpdateUseCase.onboardUser(SecurityUtils.getUserId(), userOnboardingRequestDto)));
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto<UserInfoResponseDto>> updateUser(@Valid @RequestBody final UpdatingUserInfoRequestDto updatingUserInfoRequestDto) {
+        return ResponseEntity.ok(ResponseDto.success(userUpdateUseCase.updateUser(updatingUserInfoRequestDto)));
     }
 }
