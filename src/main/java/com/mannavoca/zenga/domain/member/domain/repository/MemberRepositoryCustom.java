9a91e7b72798ae886b78756c9c686b338b02c374
mingeun0507 <tqdlfkg@naver.com>
2023-08-30 23:07:27 +0900

    [Fix] 단순 조회에서 검색 & 페이지네이션 API로 변경에 따라 쿼리 및 Mapper (List -> Slice) 수정

--- src/main/java/com/mannavoca/zenga/domain/member/domain/repository/MemberRepositoryCustom.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/repository/MemberRepositoryCustom.java
@@ -1,11 +1,18 @@
 package com.mannavoca.zenga.domain.member.domain.repository;
 
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
     List<Member> findMembersByChannelId(Long memberId, Long channelId);
 
-    List<Member> findAllMembersByChannelId(Long channelId);
+    Slice<Member> findAllMemberSlicesByChannelId(Long channelId, Long MemberIdCursor, String keyword, Pageable pageable);
+
+    boolean existsByMemberId(Long memberId);
+    boolean existsByUserIdAndChannelId(Long userId, Long channelId);
+
+    Long countMemberByChannelId(Long channelId);
 }
