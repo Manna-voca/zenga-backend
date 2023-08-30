8c97706b514bc8e34fbfa87e749dabbc6ea6f04b
mingeun0507 <tqdlfkg@naver.com>
2023-08-30 14:43:42 +0900

    [Fix] 채널 한 줄 소개 주석 처리

--- src/main/java/com/mannavoca/zenga/domain/channel/application/dto/request/CreatingChannelRequestDto.java
+++ src/main/java/com/mannavoca/zenga/domain/channel/application/dto/request/CreatingChannelRequestDto.java
@@ -3,10 +3,15 @@ package com.mannavoca.zenga.domain.channel.application.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class CreatingChannelRequestDto {
+    @NotBlank(message = "채널 이름을 입력해주세요.")
     private String name;
+    @NotBlank(message = "채널 로고 이미지 URL을 입력해주세요.")
     private String logoImageUrl;
-    private String description;
+//    @NotBlank(message = "채널 설명을 입력해주세요.")
+//    private String description;
 }
