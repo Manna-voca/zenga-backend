452288dcdd42514fdee00b2a67dc0a49fa4d3d1a
99winnmin <ojysep9987@gmail.com>
2023-08-30 14:38:59 +0900

    :art: Fix : 모임 관련 patch, post API channelId url 파라미터 모두 DTO 화

--- src/main/java/com/mannavoca/zenga/domain/party/application/dto/request/ApplyPartyRequestDto.java
+++ src/main/java/com/mannavoca/zenga/domain/party/application/dto/request/ApplyPartyRequestDto.java
@@ -4,17 +4,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EditPartyInfoRequestDto {
+public class ApplyPartyRequestDto {
     private Long channelId;
     private Long partyId;
-    private String title;
-    private String content;
-    private Integer maxCapacity;
-    private String location;
-    private LocalDateTime partyDate;
-    private String partyImageUrl;
 }
