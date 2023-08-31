c62bfbecb0e17eb0ad58d0388f3fa02e0abc1dbd
mingeun0507 <tqdlfkg@naver.com>
2023-08-31 12:31:00 +0900

    [Fix] 마이페이지 나의 모임 API 개선

--- src/main/java/com/mannavoca/zenga/domain/member/presentation/MemberController.java
+++ src/main/java/com/mannavoca/zenga/domain/member/presentation/MemberController.java
@@ -4,6 +4,7 @@ import com.mannavoca.zenga.common.dto.ResponseDto;
 import com.mannavoca.zenga.common.dto.SliceResponse;
 import com.mannavoca.zenga.common.util.SecurityUtils;
 import com.mannavoca.zenga.domain.member.application.dto.request.CreatingMemberRequestDto;
+import com.mannavoca.zenga.domain.member.application.dto.request.GetMemberPartyRequestDto;
 import com.mannavoca.zenga.domain.member.application.dto.response.MemberInfoResponseDto;
 import com.mannavoca.zenga.domain.member.application.service.MemberCreateUseCase;
 import com.mannavoca.zenga.domain.member.application.service.MemberReadUseCase;
@@ -15,37 +16,39 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Enumerated;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/members")
+@Validated
 public class MemberController {
     private final MemberCreateUseCase memberCreateUseCase;
     private final MemberReadUseCase memberReadUseCase;
 
     @PostMapping
-    public ResponseEntity<ResponseDto<MemberInfoResponseDto>> createMember(@RequestBody CreatingMemberRequestDto creatingMemberRequestDto) {
+    public ResponseEntity<ResponseDto<MemberInfoResponseDto>> createMember(@Valid @RequestBody final CreatingMemberRequestDto creatingMemberRequestDto) {
         return ResponseEntity.ok(ResponseDto.success(memberCreateUseCase.createMember(creatingMemberRequestDto)));
     }
 
     @GetMapping("/{memberId}/parties")
     public ResponseEntity<ResponseDto<SliceResponse<PartyTapResponseDto>>> getPartyList(
-            @PathVariable Long memberId,
-            @RequestParam @NotNull State state,
-            @RequestParam(required = false) Long cursor,
-            @PageableDefault(size = 9) Pageable pageable
+            @PathVariable @NotNull(message = "멤버 ID는 필수입니다.") final Long memberId,
+            @ModelAttribute final GetMemberPartyRequestDto getMemberPartyRequestDto,
+            @PageableDefault(size = 9) final Pageable pageable
     ) {
-        return ResponseEntity.ok(ResponseDto.success(SliceResponse.of(memberReadUseCase.getPartyListByMemberId(memberId, state, cursor, pageable))));
+        return ResponseEntity.ok(ResponseDto.success(SliceResponse.of(memberReadUseCase.getPartyListByMemberId(memberId, getMemberPartyRequestDto.getState(), getMemberPartyRequestDto.getCursor(), pageable))));
     }
 
     @GetMapping("/{memberId}/parties/all")
     public ResponseEntity<ResponseDto<List<PartyTapResponseDto>>> getAll2PartyList(
-            @PathVariable Long memberId
+            @PathVariable @NotNull(message = "멤버 ID는 필수입니다.") final Long memberId
     ) {
         return ResponseEntity.ok(ResponseDto.success(memberReadUseCase.getAll2PartyListByMemberId(memberId)));
     }
