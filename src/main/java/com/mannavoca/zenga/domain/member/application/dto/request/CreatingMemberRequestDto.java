df7414f2f07abb7a61eb0ac5d29446e902fcbb0f
mingeun0507 <tqdlfkg@naver.com>
2023-08-31 12:29:16 +0900

    [Fix] 프로필 사진 필수 아니도록 변경

--- src/main/java/com/mannavoca/zenga/domain/member/application/dto/request/CreatingMemberRequestDto.java
+++ src/main/java/com/mannavoca/zenga/domain/member/application/dto/request/CreatingMemberRequestDto.java
@@ -4,20 +4,23 @@ import com.mannavoca.zenga.domain.member.domain.entity.enumType.LevelType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class CreatingMemberRequestDto {
-    @NotNull(message = "채널 ID를 입력해주세요.")
+    @NotNull(message = "채널 ID는 필수입니다.")
     private Long channelId;
-    @NotBlank(message = "프로필 사진 주소를 입력해주세요.")
+//    @NotBlank(message = "프로필 사진 링크는 필수입니다.")
     private String profileImageUrl;
-    @NotBlank(message = "이름을 입력해주세요.")
+    @NotBlank(message = "이름은 필수입니다.")
     private String nickname;
-    @NotBlank(message = "한줄소개를 입력해주세요.")
+    @NotNull(message = "한줄소개는 필수입니다.")
     private String introduction;
-    @NotBlank(message = "권한을 입력해주세요.")
+    @NotNull(message = "권한을 입력해주세요.")
+    @Enumerated(value = EnumType.STRING)
     private LevelType level;
 }
