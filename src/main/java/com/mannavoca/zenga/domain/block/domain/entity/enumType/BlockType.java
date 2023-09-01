d5d7a4bbb1c8b864507beb5c1c8623658124db8a
mingeun0507 <tqdlfkg@naver.com>
2023-09-01 22:56:23 +0900

    [Feat] Block 관련 Repository & Service 및 enum, Mapper, 정의

--- src/main/java/com/mannavoca/zenga/domain/block/domain/entity/enumType/BlockType.java
+++ src/main/java/com/mannavoca/zenga/domain/block/domain/entity/enumType/BlockType.java
@@ -1,37 +1,50 @@
 package com.mannavoca.zenga.domain.block.domain.entity.enumType;
 
-
+import com.mannavoca.zenga.common.exception.BusinessException;
+import com.mannavoca.zenga.common.exception.Error;
 import com.mannavoca.zenga.common.infrastructure.domain.CodeValue;
+import com.mannavoca.zenga.domain.praise.domain.entity.enumType.PraiseType;
 
 public enum BlockType implements CodeValue {
 
-    // 확장성을 고려해 한 글자가 아닌 색 이름으로 변경
-    PINK("PINK", "분홍색", "외적 칭찬"),
-    ORANGE("ORANGE","주황색", "성격 칭찬"),
-    SKY_BLUE("SKY_BLUE","하늘색", "도전/열정 칭찬"),
-    LIGHT_GREEN("LIGHT_GREEN","연두색", "기타 칭찬"),
-    YELLOW("YELLOW","노란색", "모임 참여"),
-    PURPLE("PURPLE","보라색", "노력 칭찬"),
-    LIGHT_BROWN("LIGHT_BROWN","연갈색", "칭찬하기")
-
-    ;
-
+    PINK("PINK", "분홍색", PraiseType.APPEARANCE),
+    ORANGE("ORANGE", "주황색", PraiseType.PERSONALITY),
+    SKY_BLUE("SKY_BLUE", "하늘색", PraiseType.PASSION),
+    LIGHT_GREEN("LIGHT_GREEN", "연두색", PraiseType.OTHERS),
+    YELLOW("YELLOW", "노란색", PraiseType.EFFORT),
+    PURPLE("PURPLE", "보라색", PraiseType.EFFORT),
+    LIGHT_BROWN("LIGHT_BROWN", "연갈색", PraiseType.EFFORT);
 
     private String code;
     private String value;
-    private String category;
+    private PraiseType category;
 
-    BlockType(String code, String value, String category) {
+    BlockType(String code, String value, PraiseType category) {
         this.code = code;
         this.value = value;
         this.category = category;
     }
+
     @Override
     public String getCode() {
         return code;
     }
+
     @Override
     public String getValue() {
         return value;
+    }
+
+    public PraiseType getCategory() {
+        return category;
+    }
+
+    public static BlockType fromPraiseType(PraiseType praiseType) {
+        for (BlockType blockType : BlockType.values()) {
+            if (blockType.getCategory() == praiseType) {
+                return blockType;
+            }
+        }
+        throw BusinessException.of(Error.INTERNAL_SERVER_ERROR);
     }
 }
