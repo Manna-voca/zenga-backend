83010ce4c6a106d31c3feeb663ec4326b68894bf
mingeun0507 <tqdlfkg@naver.com>
2023-09-01 22:59:02 +0900

    [Chore] Async 활성화

--- src/main/java/com/mannavoca/zenga/ZengaApplication.java
+++ src/main/java/com/mannavoca/zenga/ZengaApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableBatchProcessing
+@EnableAsync
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties({JwtProperties.class, OAuthProperties.class})
