057c82fee162521900f1ef1d56e36b230201f4ba
mingeun0507 <tqdlfkg@naver.com>
2023-09-03 17:40:37 +0900

    [Fix] BlockType에 추가 상수 정의

--- src/main/java/com/mannavoca/zenga/domain/block/domain/entity/enumType/BlockType.java
+++ src/main/java/com/mannavoca/zenga/domain/block/domain/entity/enumType/BlockType.java
@@ -7,13 +7,26 @@ import com.mannavoca.zenga.domain.praise.domain.entity.enumType.PraiseType;
 
 public enum BlockType implements CodeValue {
 
-    PINK("PINK", "외모 칭찬", PraiseType.APPEARANCE),
-    ORANGE("ORANGE", "인격 칭찬", PraiseType.PERSONALITY),
-    SKY_BLUE("SKY_BLUE", "도전/열정 칭찬", PraiseType.PASSION),
-    LIGHT_GREEN("LIGHT_GREEN", "기타 칭찬", PraiseType.OTHERS),
-    YELLOW("YELLOW", "모임 참여", null),
-    PURPLE("PURPLE", "노력 칭찬", PraiseType.EFFORT),
-    LIGHT_BROWN("LIGHT_BROWN", "칭찬하기", PraiseType.EFFORT);
+    PINK("PINK", "외적 칭찬을 받았어요", PraiseType.APPEARANCE),
+    ORANGE("ORANGE", "성격 칭찬을 받았어요", PraiseType.PERSONALITY),
+    SKY_BLUE("SKY_BLUE", "도전﹒열정 칭찬을 받았어요", PraiseType.PASSION),
+    LIGHT_GREEN("LIGHT_GREEN", "그 외 칭찬을 받았어요", PraiseType.OTHERS),
+    YELLOW1("YELLOW1", "첫 모임에 참여했어요", null),
+    YELLOW10("YELLOW10", "모임에 총 10번 참여했어요", null),
+    YELLOW30("YELLOW30", "모임에 총 30번 참여했어요", null),
+    YELLOW50("YELLOW50", "모임에 총 50번 참여했어요", null),
+    PURPLE("PURPLE", "능력 칭찬을 받았어요", PraiseType.ABILITY),
+    LIGHT_BROWN1("LIGHT_BROWN1", "첫 칭찬을 보냈어요", null),
+    LIGHT_BROWN10("LIGHT_BROWN10", "칭찬을 총 10번 보냈어요", null),
+    LIGHT_BROWN50("LIGHT_BROWN50", "칭찬을 총 50번 보냈어요", null),
+    LIGHT_BROWN100("LIGHT_BROWN100", "칭찬을 총 100번 보냈어요", null),
+    PINK_FIRST("PINK_FIRST", "첫 칭찬을 받았어요", null),
+    ORANGE_FIRST("ORANGE_FIRST", "첫 칭찬을 받았어요", null),
+    SKY_BLUE_FIRST("SKY_BLUE_FIRST", "첫 칭찬을 받았어요", null),
+    LIGHT_GREEN_FIRST("LIGHT_GREEN_FIRST", "첫 칭찬을 받았어요", null),
+    PURPLE_FIRST("PURPLE_FIRST", "첫 칭찬을 받았어요", null)
+
+    ;
 
     private String code;
     private String value;
@@ -46,5 +59,22 @@ public enum BlockType implements CodeValue {
             }
         }
         throw BusinessException.of(Error.INTERNAL_SERVER_ERROR);
+    }
+
+    public static BlockType getFirstOneFromBlockType(BlockType blockType) {
+        switch (blockType) {
+            case PINK:
+                return PINK_FIRST;
+            case ORANGE:
+                return ORANGE_FIRST;
+            case SKY_BLUE:
+                return SKY_BLUE_FIRST;
+            case LIGHT_GREEN:
+                return LIGHT_GREEN_FIRST;
+            case PURPLE:
+                return PURPLE_FIRST;
+            default:
+                throw BusinessException.of(Error.INTERNAL_SERVER_ERROR);
+        }
     }
 }
