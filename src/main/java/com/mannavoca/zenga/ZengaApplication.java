247fdca8f1a6c6f8ee6aaa617dbfeb08abc52dcf
99winnmin <ojysep9987@gmail.com>
2023-09-05 23:11:15 +0900

    :fire: Fix : Batch 작업 삭제

--- src/main/java/com/mannavoca/zenga/ZengaApplication.java
+++ src/main/java/com/mannavoca/zenga/ZengaApplication.java
@@ -2,13 +2,11 @@ package com.mannavoca.zenga;
 
 import com.mannavoca.zenga.common.security.jwt.JwtProperties;
 import com.mannavoca.zenga.common.security.oauth.OAuthProperties;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableBatchProcessing
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties({JwtProperties.class, OAuthProperties.class})
