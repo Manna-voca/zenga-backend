8070915bc01b06bfd6a2c4e75cf1f3111fccc71f
mingeun0507 <tqdlfkg@naver.com>
2023-09-06 09:16:58 +0900

    [Fix] 불필요한 @EnableAsync 어노테이션 제거

--- src/main/java/com/mannavoca/zenga/ZengaApplication.java
+++ src/main/java/com/mannavoca/zenga/ZengaApplication.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableBatchProcessing
-@EnableAsync
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties({JwtProperties.class, OAuthProperties.class})
