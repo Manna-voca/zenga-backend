5cb4454ecfdaefeb4a9b37c459d996dd7a4aacc5
mingeun0507 <tqdlfkg@naver.com>
2023-09-11 09:39:24 +0900

    [Feat] 임원진 체크 과정 추가

--- src/main/java/com/mannavoca/zenga/domain/channel/application/service/ChannelUpdateUseCase.java
+++ src/main/java/com/mannavoca/zenga/domain/channel/application/service/ChannelUpdateUseCase.java
@@ -1,11 +1,17 @@
 package com.mannavoca.zenga.domain.channel.application.service;
 
 import com.mannavoca.zenga.common.annotation.UseCase;
+import com.mannavoca.zenga.common.exception.BusinessException;
+import com.mannavoca.zenga.common.exception.Error;
+import com.mannavoca.zenga.common.util.UserUtils;
 import com.mannavoca.zenga.domain.channel.application.dto.request.UpdatingChannelRequestDto;
 import com.mannavoca.zenga.domain.channel.application.dto.response.ChannelResponseDto;
 import com.mannavoca.zenga.domain.channel.application.mapper.ChannelMapper;
 import com.mannavoca.zenga.domain.channel.domain.entity.Channel;
 import com.mannavoca.zenga.domain.channel.domain.service.ChannelService;
+import com.mannavoca.zenga.domain.member.domain.entity.Member;
+import com.mannavoca.zenga.domain.member.domain.entity.enumType.LevelType;
+import com.mannavoca.zenga.domain.member.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,9 +20,15 @@ import lombok.extern.slf4j.Slf4j;
 @UseCase
 public class ChannelUpdateUseCase {
     private final ChannelService channelService;
+    private final MemberService memberService;
+    private final UserUtils userUtils;
 
     public ChannelResponseDto updateChannel(final Long channelId, final UpdatingChannelRequestDto updatingChannelRequestDto) {
         Channel channel = channelService.getChannelById(channelId);
+        Member currentMember = userUtils.getMember(channelId);
+        if (!currentMember.getLevel().equals(LevelType.MAINTAINER)){
+            throw BusinessException.of(Error.NOT_MAINTAINER_OF_CHANNEL);
+        }
 
         return ChannelMapper.mapChannelToChannelResponseDto(channelService.updateChannel(channel, updatingChannelRequestDto));
     }
