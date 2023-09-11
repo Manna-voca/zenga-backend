5f90e274628cfc2ade55e0aff63d369139d4ae2e
99winnmin <ojysep9987@gmail.com>
2023-09-11 18:26:31 +0900

    :sparkles: Feat : 모임 댓글 갯수 숫자 및 가장 최신 댓글 관련 도메인 메서드, 쿼리 추가

--- src/main/java/com/mannavoca/zenga/domain/comment/domain/repository/CommentRepositoryCustom.java
+++ src/main/java/com/mannavoca/zenga/domain/comment/domain/repository/CommentRepositoryCustom.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepositoryCustom {
     Slice<Comment> findAllParentCommentInParty(Long partyId, Long commentId, Pageable pageable);
     List<Comment> findAllChildCommentInParty(Long parentId);
+    Long countPartyCommentAmount(Long partyId);
+    Optional<Comment> findLastCommentByPartyId(Long partyId);
 }
