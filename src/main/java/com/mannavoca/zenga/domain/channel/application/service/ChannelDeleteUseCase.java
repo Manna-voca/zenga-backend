61c91af8156a8e7f0980d7cef71ba29e190b6bdb
mingeun0507 <tqdlfkg@naver.com>
2023-09-21 09:02:24 +0900

    fix: 시간 설정 변경 및 @Transactional 어노테이션 누락된 것 수정

--- src/main/java/com/mannavoca/zenga/domain/channel/application/service/ChannelDeleteUseCase.java
+++ src/main/java/com/mannavoca/zenga/domain/channel/application/service/ChannelDeleteUseCase.java
@@ -7,13 +7,16 @@ import com.mannavoca.zenga.common.util.UserUtils;
 import com.mannavoca.zenga.domain.channel.domain.service.ChannelService;
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import com.mannavoca.zenga.domain.member.domain.entity.enumType.LevelType;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
 @RequiredArgsConstructor
 @Slf4j
 @UseCase
+@Transactional(propagation = REQUIRES_NEW)
 public class ChannelDeleteUseCase {
     private final ChannelService channelService;
     private final UserUtils userUtils;
