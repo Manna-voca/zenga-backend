288ee46577717b34617e7c8e6a7a114b95318df9
99winnmin <ojysep9987@gmail.com>
2024-03-25 02:17:30 +0900

    :art: Fix : POST /praise/todo 수정
    Resolved: #78

--- src/main/java/com/mannavoca/zenga/domain/praise/presentation/PraisePostController.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/presentation/PraisePostController.java
@@ -1,28 +1,34 @@
 package com.mannavoca.zenga.domain.praise.presentation;
 
-import com.mannavoca.zenga.common.dto.ResponseDto;
-import com.mannavoca.zenga.domain.praise.application.dto.request.PraiseChannelIdRequestDto;
-import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
-import com.mannavoca.zenga.domain.praise.application.service.PraiseCreateUseCase;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mannavoca.zenga.common.dto.ResponseDto;
+import com.mannavoca.zenga.common.util.UserUtils;
+import com.mannavoca.zenga.domain.member.domain.entity.Member;
+import com.mannavoca.zenga.domain.praise.application.dto.request.PraiseChannelIdRequestDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
+import com.mannavoca.zenga.domain.praise.application.service.PraiseCreateUseCase;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @RestController
 @RequestMapping(value="/praise")
 @RequiredArgsConstructor
 public class PraisePostController {
+    private final UserUtils userUtils;
     private final PraiseCreateUseCase praiseCreateUseCase;
 
     @PostMapping("/todo")
     public ResponseEntity<ResponseDto<CurrentTodoPraiseResponseDto>> getMyCurrentToDoPraise(@RequestBody PraiseChannelIdRequestDto praiseChannelIdRequestDto)
     {
-        CurrentTodoPraiseResponseDto currentTodoPraiseResponseDto = praiseCreateUseCase.getCurrentTodoPraiseAndMemberList(praiseChannelIdRequestDto.getChannelId());
+        Member member = userUtils.getMember(praiseChannelIdRequestDto.getChannelId());
+        CurrentTodoPraiseResponseDto currentTodoPraiseResponseDto = praiseCreateUseCase.getCurrentTodoPraiseAndMemberList(member.getId(), praiseChannelIdRequestDto.getChannelId());
         return ResponseEntity.ok(ResponseDto.success(currentTodoPraiseResponseDto));
     }
 
