e80d948b1d9b8affe0cddac2b3a3f4a25f9eb5f9
99winnmin <ojysep9987@gmail.com>
2024-03-25 02:10:10 +0900

    :zap: Perf : 셔플할때 동적으로 셔플 제한 횟수 검증하도록 변경

--- src/main/java/com/mannavoca/zenga/domain/praise/application/service/PraiseUpdateUseCase.java
+++ src/main/java/com/mannavoca/zenga/domain/praise/application/service/PraiseUpdateUseCase.java
@@ -1,29 +1,32 @@
 package com.mannavoca.zenga.domain.praise.application.service;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.mannavoca.zenga.common.annotation.UseCase;
+import com.mannavoca.zenga.common.consts.ShuffleEstimationConstants;
 import com.mannavoca.zenga.common.exception.BusinessException;
 import com.mannavoca.zenga.common.exception.Error;
 import com.mannavoca.zenga.common.util.UserUtils;
 import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import com.mannavoca.zenga.domain.member.domain.service.MemberService;
-import com.mannavoca.zenga.domain.notification.domain.entity.Notification;
 import com.mannavoca.zenga.domain.notification.domain.service.NotificationService;
 import com.mannavoca.zenga.domain.point.application.service.PointPolicyUseCase;
 import com.mannavoca.zenga.domain.praise.application.dto.event.PraisedMemberEventDto;
 import com.mannavoca.zenga.domain.praise.application.dto.request.ChooseMemberPraiseRequestDto;
 import com.mannavoca.zenga.domain.praise.application.dto.request.OpenMemberPraiseRequestDto;
 import com.mannavoca.zenga.domain.praise.application.dto.response.CurrentTodoPraiseResponseDto;
+import com.mannavoca.zenga.domain.praise.application.dto.response.ReceivedPraiseInfoResponseDto;
 import com.mannavoca.zenga.domain.praise.application.mapper.PraiseMapper;
 import com.mannavoca.zenga.domain.praise.domain.entity.MemberPraise;
 import com.mannavoca.zenga.domain.praise.domain.service.CandidateService;
 import com.mannavoca.zenga.domain.praise.domain.service.MemberPraiseService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Slf4j
 @UseCase
@@ -43,7 +46,11 @@ public class PraiseUpdateUseCase {
         Optional<MemberPraise> currentTodoPraiseOpt = memberPraiseService.findCurrentTodoPraiseForMember(member.getId());
 
         if (currentTodoPraiseOpt.isPresent() && currentTodoPraiseOpt.get().getPraisedMember() == null) {
-            if (currentTodoPraiseOpt.get().getShuffleCount() == 1) { throw BusinessException.of(Error.CANNOT_SHUFFLE); }
+            // 셔플 제한 횟수 계산
+            Long channelMemberCount = memberService.countMemberByChannelId(channelId);
+            int limitShuffleCount = ShuffleEstimationConstants.determineLimitShuffleCount(channelMemberCount);
+            if (currentTodoPraiseOpt.get().getShuffleCount() >= limitShuffleCount-1) { throw BusinessException.of(Error.CANNOT_SHUFFLE); }
+
             List<Member> candidates = candidateService.getCandidates(member.getId());
             memberPraiseService.updatePlusShuffleCountMemberPraise(currentTodoPraiseOpt.get());
             return PraiseMapper.mapToCurrentTodoPraiseResponseDto(currentTodoPraiseOpt.get(), candidates);
@@ -65,13 +72,24 @@ public class PraiseUpdateUseCase {
         praiseUpdateEventListener.updatePraisedMemberBlock(PraisedMemberEventDto.of(praisedMember.getId(), memberPraise.getPraise().getCategory()));
     }
 
-    public void openMemberPraise(OpenMemberPraiseRequestDto openMemberPraiseRequestDto) {
+    public ReceivedPraiseInfoResponseDto openMemberPraise(OpenMemberPraiseRequestDto openMemberPraiseRequestDto) {
         Member member = userUtils.getMember(openMemberPraiseRequestDto.getChannelId());
         MemberPraise memberPraise = memberPraiseService.findMemberPraiseById(openMemberPraiseRequestDto.getMemberPraiseId());
+        if (memberPraise.getIsOpen()) { throw BusinessException.of(Error.ALREADY_OPEN); }
         if (!Objects.equals(memberPraise.getPraisedMember().getId(), member.getId())) { throw BusinessException.of(Error.NOT_MEMBER_PRAISE_OWNER); }
 
         pointPolicyUseCase.usePoint(member, member.getChannel().getName());
 
         memberPraiseService.updateOpenMemberPraise(memberPraise);
+
+        return ReceivedPraiseInfoResponseDto.builder()
+            .memberPraiseId(memberPraise.getId())
+            .isOpened(memberPraise.getIsOpen())
+            .praiseDescription(memberPraise.getPraise().getDescription())
+            .praiseType(memberPraise.getPraise().getCategory())
+            .memberId(memberPraise.getPraiseMember().getId())
+            .memberName(memberPraise.getPraiseMember().getNickname())
+            .memberProfileImageUrl(memberPraise.getPraiseMember().getProfileImageUrl())
+            .build();
     }
 }
