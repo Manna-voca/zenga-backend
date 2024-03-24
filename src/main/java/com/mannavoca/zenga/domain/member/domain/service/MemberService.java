747686be4424cf08caede9c23bb73aaae419b899
99winnmin <ojysep9987@gmail.com>
2024-03-25 02:06:06 +0900

    :zap: Fix : 칭찬받을 사람 후보자 메서드 동적으로 랜덤으로 가져오도록 변경

--- src/main/java/com/mannavoca/zenga/domain/member/domain/service/MemberService.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/service/MemberService.java
@@ -1,6 +1,13 @@
 package com.mannavoca.zenga.domain.member.domain.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.mannavoca.zenga.common.annotation.DomainService;
+import com.mannavoca.zenga.common.consts.ShuffleEstimationConstants;
 import com.mannavoca.zenga.common.exception.BusinessException;
 import com.mannavoca.zenga.common.exception.Error;
 import com.mannavoca.zenga.domain.channel.domain.entity.Channel;
@@ -12,22 +19,16 @@ import com.mannavoca.zenga.domain.member.domain.entity.Member;
 import com.mannavoca.zenga.domain.member.domain.entity.enumType.LevelType;
 import com.mannavoca.zenga.domain.member.domain.repository.MemberRepository;
 import com.mannavoca.zenga.domain.user.domain.entity.User;
-import com.mannavoca.zenga.domain.user.domain.service.UserFindService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
-import java.util.List;
 
 @Slf4j
 @DomainService
 @RequiredArgsConstructor
 public class MemberService {
+
     private final MemberRepository memberRepository;
-    private final UserFindService userFindService;
 
     public Member getChannelMaker(Long channelId){
         return memberRepository.findMemberByChannel_IdAndLevel(channelId, LevelType.MAINTAINER)
@@ -44,12 +45,11 @@ public class MemberService {
                 .orElseThrow(() -> BusinessException.of(Error.NOT_MEMBER_OF_CHANNEL));
     }
 
-    public List<Member> find8RandomMembersByChannelId(Long memberId, Long channelId) {
-        List<Member> membersByClubId = memberRepository.findMembersByChannelId(memberId, channelId);
-
-        Collections.shuffle(membersByClubId);  // 리스트를 랜덤하게 섞는다.
-        // 만약 리스트의 크기가 4 보다 작다면, 리스트의 모든 멤버를 반환한다.
-        return membersByClubId.subList(0, Math.min(membersByClubId.size(), 8));
+    public List<Member> findDynamicRandomMembersByChannelId(Long memberId, Long channelId) {
+        Long memberCount = countMemberByChannelId(channelId);
+        if (memberCount < 8) throw BusinessException.of(Error.NOT_ENOUGH_MEMBER);
+        int limit = ShuffleEstimationConstants.determineLimit(memberCount);
+        return memberRepository.findRandomMembersByChannelId(memberId, channelId, limit);
     }
 
     public Member findMemberById(Long memberId) {
