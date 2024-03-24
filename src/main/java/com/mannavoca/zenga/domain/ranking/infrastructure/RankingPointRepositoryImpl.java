a821ed1844734dc6286d687e3cf940d33f5a4035
mingeun0507 <tqdlfkg@naver.com>
2024-03-25 01:42:45 +0900

    fix: 랭킹 순위 집계 오류 수정

--- src/main/java/com/mannavoca/zenga/domain/ranking/infrastructure/RankingPointRepositoryImpl.java
+++ src/main/java/com/mannavoca/zenga/domain/ranking/infrastructure/RankingPointRepositoryImpl.java
@@ -13,19 +13,31 @@ import java.util.List;
 public class RankingPointRepositoryImpl implements RankingPointCustomRepository {
     private static final String GET_MEMBER_RANK_QUERY =
             "SELECT\n" +
-                    "    DENSE_RANK() OVER (ORDER BY point DESC, m.nickname) AS member_rank,\n" +
-                    "    m.id AS member_id,\n" +
-                    "    m.profile_image_url AS profile_image_url,\n" +
-                    "    m.nickname AS nickname,\n" +
-                    "    COALESCE(SUM(r.point), 0) AS point \n" +
-                    "FROM\n" +
-                    "    zg_member m\n" +
-                    "LEFT JOIN\n" +
-                    "    zg_ranking_point r ON m.id = r.member_id\n" +
-                    "WHERE\n" +
-                    "    m.id = ?\n" +
-                    "GROUP BY\n" +
-                    "    m.id, m.profile_image_url, m.nickname";
+                    "    member_rank,\n" +
+                    "    member_id,\n" +
+                    "    profile_image_url,\n" +
+                    "    nickname,\n" +
+                    "    point\n" +
+                    "FROM (\n" +
+                    "    SELECT\n" +
+                    "        DENSE_RANK() OVER (ORDER BY COALESCE(SUM(r.point), 0) DESC, m.nickname) AS member_rank,\n" +
+                    "        m.id AS member_id,\n" +
+                    "        m.profile_image_url AS profile_image_url,\n" +
+                    "        m.nickname AS nickname,\n" +
+                    "        COALESCE(SUM(r.point), 0) AS point \n" +
+                    "    FROM\n" +
+                    "        zg_member m\n" +
+                    "    LEFT JOIN\n" +
+                    "        zg_ranking_point r ON m.id = r.member_id\n" +
+                    "    WHERE\n" +
+                    "        m.id = ?\n" +
+                    "    GROUP BY\n" +
+                    "        m.id, m.profile_image_url, m.nickname\n" +
+                    ") AS rankedMembers\n" +
+                    "ORDER BY\n" +
+                    "    member_rank, nickname"; // 이 부분이 수정되었습니다.
+
+
 
     private static final String GET_CHANNEL_MEMBER_RANKS_QUERY =
             "SELECT\n" +
@@ -36,7 +48,7 @@ public class RankingPointRepositoryImpl implements RankingPointCustomRepository 
                     "    point\n" +
                     "FROM (\n" +
                     "    SELECT\n" +
-                    "        DENSE_RANK() OVER (ORDER BY point DESC, m.nickname) AS member_rank,\n" +
+                    "        DENSE_RANK() OVER (ORDER BY COALESCE(SUM(r.point), 0) DESC, m.nickname) AS member_rank,\n" +
                     "        m.id AS member_id,\n" +
                     "        m.profile_image_url AS profile_image_url,\n" +
                     "        m.nickname AS nickname,\n" +
@@ -48,16 +60,17 @@ public class RankingPointRepositoryImpl implements RankingPointCustomRepository 
                     "    INNER JOIN\n" +
                     "        zg_channel c ON m.channel_id = c.id\n" +
                     "    WHERE\n" +
-                    "        channel_id = ?\n" +
+                    "        c.id = ?\n" +
                     "    GROUP BY\n" +
                     "        m.id, m.profile_image_url, m.nickname\n" +
                     ") AS rankedMembers\n" +
-                    "    WHERE\n" +
-                    "        point > 0\n" +
-                    "    ORDER BY\n" +
-                    "        member_rank, nickname\n" +
-                    "    limit\n" +
-                    "        20";
+                    "WHERE\n" +
+                    "    point > 0\n" +
+                    "ORDER BY\n" +
+                    "    member_rank, nickname\n" +
+                    "LIMIT\n" +
+                    "    20";
+
 
     private final JdbcTemplate jdbcTemplate;
 
