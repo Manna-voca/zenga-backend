18ac7b3859e9cc14c064bbefca31837eff85ab5b
mingeun0507 <tqdlfkg@naver.com>
2024-03-24 19:30:59 +0900

    feat: 채널 전체 랭킹 조회 API 구현

--- src/main/java/com/mannavoca/zenga/domain/ranking/domain/repository/RankingPointCustomRepository.java
+++ src/main/java/com/mannavoca/zenga/domain/ranking/domain/repository/RankingPointCustomRepository.java
@@ -2,6 +2,10 @@ package com.mannavoca.zenga.domain.ranking.domain.repository;
 
 import com.mannavoca.zenga.domain.ranking.application.dto.MemberRankDto;
 
+import java.util.List;
+
 public interface RankingPointCustomRepository {
     MemberRankDto findMemberRank(Long memberId);
+
+    List<MemberRankDto> findChannelMemberRanks(Long channelId);
 }
--- src/main/java/com/mannavoca/zenga/domain/ranking/presentation/RankingPointController.java
+++ src/main/java/com/mannavoca/zenga/domain/ranking/presentation/RankingPointController.java
@@ -1,6 +1,7 @@
 package com.mannavoca.zenga.domain.ranking.presentation;
 
 import com.mannavoca.zenga.common.dto.ResponseDto;
+import com.mannavoca.zenga.domain.ranking.application.dto.ChannelMemberRankDto;
 import com.mannavoca.zenga.domain.ranking.application.dto.MemberRankDto;
 import com.mannavoca.zenga.domain.ranking.application.service.RankingPointReadUseCase;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/ranking")
@@ -19,7 +23,17 @@ public class RankingPointController {
     private final RankingPointReadUseCase rankingPointReadUseCase;
 
     @GetMapping("/my")
-    public ResponseEntity<ResponseDto<MemberRankDto>> findMyRank(@RequestParam("channelId") final Long channelId) {
+    public ResponseEntity<ResponseDto<MemberRankDto>> findMyRank(
+            @Valid @NotNull @RequestParam("channelId") final Long channelId
+    ) {
         return ResponseEntity.ok(ResponseDto.success(rankingPointReadUseCase.findMyRank(channelId)));
     }
+
+    @GetMapping
+    public ResponseEntity<ResponseDto<ChannelMemberRankDto>> findChannelMemberRank(
+            @Valid @NotNull @RequestParam("channelId") final Long channelId
+    ) {
+        return ResponseEntity.ok(ResponseDto.success(rankingPointReadUseCase.findChannelMemberRanks(channelId)));
+    }
+
 }
