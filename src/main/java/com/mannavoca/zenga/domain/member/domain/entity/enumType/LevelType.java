167a94a31cf9b9316a181b660ec4479ae7db16d5
99winnmin <ojysep9987@gmail.com>
2024-03-24 19:50:45 +0900

    :egg: Fix : 멤버 타입에 ADMIN 추가
    수정사항 : 이벤트 기간동안 이벤트 관리자 타입 추가

--- src/main/java/com/mannavoca/zenga/domain/member/domain/entity/enumType/LevelType.java
+++ src/main/java/com/mannavoca/zenga/domain/member/domain/entity/enumType/LevelType.java
@@ -5,7 +5,8 @@ import com.mannavoca.zenga.common.infrastructure.domain.CodeValue;
 
 public enum LevelType implements CodeValue {
     NORMAL("N", "동아리원"),
-    MAINTAINER("M","임원진");
+    MAINTAINER("M","임원진"),
+    ADMIN("A","관리자");
 
     private String code;
     private String value;
